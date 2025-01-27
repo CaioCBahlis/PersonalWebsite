1adde976211eed78897bb7612dd138cd7544967d
Caio Carraro Bahlis <caiobahlis@eduroam-226-49-166.laptops.usf.edu>
2025-01-27 03:38:49 -0500

    Minor Changes

--- Static/Landing_Page/test_me.js
+++ Static/Landing_Page/test_me.js
@@ -4,9 +4,24 @@ const desc = document.querySelectorAll(".hidden-desc")
 dropboxes.forEach((dropbox, index) => {
     dropbox.addEventListener("click", () => {
         if (dropbox.style.height === "14vh"){
-            dropbox.style.height = "70vh"
+
+
+
             desc[index].style.display = "flex"
             dropbox.style.flexDirection = "column"
+
+            let height = dropbox.offsetHeight;
+            dropbox.style.height = "auto";
+            let autoHeight = dropbox.offsetHeight;
+            dropbox.style.height = `${height}px`;
+
+
+            void dropbox.offsetHeight;
+
+
+            dropbox.style.transition = "height 0.5s smooth";
+            dropbox.style.height = `${autoHeight}px`;
+
         }else{
             dropbox.style.height = "14vh"
             desc[index].style.display = "none"
