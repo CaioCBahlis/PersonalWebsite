d45c11f64935a8e67ae0fb7f90b2ae15cf70416c
Caio Carraro Bahlis <caiobahlis@eduroam-226-192-56.laptops.usf.edu>
2025-01-28 22:39:47 -0500

    Removed Videos I still have to add

--- Static/Landing_Page/test_me.js
+++ Static/Landing_Page/test_me.js
@@ -1,30 +1,40 @@
 const dropboxes = document.querySelectorAll(".Project-Box");
 const desc = document.querySelectorAll(".hidden-desc")
 
-dropboxes.forEach((dropbox, index) => {
-    dropbox.addEventListener("click", () => {
-        if (dropbox.style.height === "14vh"){
+document.addEventListener("DOMContentLoaded", () => {
+    dropboxes.forEach((dropbox, index) => {
+        dropbox.dataset.expanded = "false";
+        dropbox.addEventListener("click", () => {
+            const computedHeight = window.getComputedStyle(dropbox).height;
+            if (dropbox.dataset.expanded === "false") {
 
 
+                desc[index].style.display = "flex"
+                dropbox.style.flexDirection = "column"
+                dropbox.style.cursor = "auto"
+                dropbox.querySelector("h3").style.paddingTop = "2rem"
 
-            desc[index].style.display = "flex"
-            dropbox.style.flexDirection = "column"
-
-            let height = dropbox.offsetHeight;
-            dropbox.style.height = "auto";
-            let autoHeight = dropbox.offsetHeight;
-            dropbox.style.height = `${height}px`;
-
-
-            void dropbox.offsetHeight;
+                let height = dropbox.offsetHeight;
+                dropbox.style.height = "auto";
+                let autoHeight = dropbox.offsetHeight;
+                dropbox.style.height = `${height}px`;
 
 
-            dropbox.style.transition = "height 0.5s smooth";
-            dropbox.style.height = `${autoHeight}px`;
+                void dropbox.offsetHeight;
 
-        }else{
-            dropbox.style.height = "14vh"
-            desc[index].style.display = "none"
-        }
+
+                dropbox.style.transition = "height 0.7s smooth";
+                dropbox.style.height = `${autoHeight}px`;
+                dropbox.dataset.expanded = "true"
+
+            } else {
+                dropbox.style.height = "14vh"
+                desc[index].style.display = "none"
+                 dropbox.dataset.expanded = "false"
+                 dropbox.style.cursor = "cursor"
+                dropbox.querySelector("h3").style.paddingTop  = "0"
+
+            }
+        });
     });
-});
+})
